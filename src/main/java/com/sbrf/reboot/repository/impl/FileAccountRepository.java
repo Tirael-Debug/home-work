b51d74502c859cfcbcd48208e8ce5750d69fbd98
[tirael] <nastya.levina@mail.ru>
2021-12-30 23:52:48 +0300

    Homework4

---
+++ src/main/java/com/sbrf/reboot/repository/impl/FileAccountRepository.java
@@ -0,0 +1,41 @@
+package com.sbrf.reboot.repository.impl;
+
+import com.sbrf.reboot.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+public class FileAccountRepository implements AccountRepository {
+
+    private final String filePath;
+
+    private static final String CLIENT_ID = "\"clientId\": ";
+
+    private static final String NUMBER = "\"number\": ";
+
+    @Override
+    public Set<Long> getAllAccountsByClientId(long clientId) throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+        String line;
+        long client = 0L;
+        Set<Long> accounts = new HashSet<>();
+        try {
+            while ((line = reader.readLine()) != null) {
+                if (line.indexOf(CLIENT_ID) > 0) {
+                    String parsedId = line.split(CLIENT_ID)[1].replaceAll(",", "");
+                    client = Long.parseLong(parsedId);
+                } else if (line.indexOf(NUMBER) > 0 && client == clientId) {
+                    String parsedNum = line.split(NUMBER)[1];
+                    accounts.add(Long.parseLong(parsedNum));
+                }
+            }
+        } catch (IOException ex) {
+            return null;
+        }
+
+        return accounts.size() > 0 ? accounts : null;
+    }
+}
