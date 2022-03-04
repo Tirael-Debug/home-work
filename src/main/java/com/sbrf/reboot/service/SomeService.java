c79e0947e509e053f93b9630beeff26899612d5f
[tirael] <nastya.levina@mail.ru>
2022-03-04 21:37:48 +0300

    Home work 14 someService fix

--- src/main/java/com/sbrf/reboot/service/SomeService.java
+++ src/main/java/com/sbrf/reboot/service/SomeService.java
@@ -16,23 +16,20 @@ public class SomeService {
         final Duration timeout = Duration.ofSeconds(5);
         ExecutorService executor = Executors.newSingleThreadExecutor();
 
-        final Future handler = executor.submit(new Callable() {
-            @Override
-            public String call() throws Exception {
+        final Future handler = executor.submit((Callable) () -> {
 
-                // Реализуйте отправку отчета используя CompletableFuture
-                CompletableFuture<String> reportResult = CompletableFuture.supplyAsync(() ->
-                        reportService.sendReport("Отправляю отчет"));
+            // Реализуйте отправку отчета используя CompletableFuture
+            CompletableFuture<String> reportResult = CompletableFuture.supplyAsync(() ->
+                    reportService.sendReport("Отправляю отчет"));
 
-                //какой то код..
-                Thread.sleep(Duration.ofSeconds(3).toMillis());
+            //какой то код..
+            Thread.sleep(Duration.ofSeconds(3).toMillis());
 
-                if (reportResult.equals("SUCCESS")) {
-                    System.out.println("Отчет отправлен успешно");
-                }
-
-                return "some return";
+            if (reportResult.get().equals("SUCCESS")) {
+                System.out.println("Отчет отправлен успешно");
             }
+
+            return "some return";
         });
 
         handler.get(timeout.toMillis(), TimeUnit.MILLISECONDS);
